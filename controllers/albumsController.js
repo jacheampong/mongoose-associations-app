f55eb6c0d88e645ef39704b5a3142d2d44e4a52e
jacheampong <jacheampong@gmail.com>
2020-10-26 23:58:32 -0400

    add album index page and controller

--- controllers/albumsController.js
+++ controllers/albumsController.js
@@ -7,6 +7,15 @@ router.get('/new', (req, res) => {
 res.render('albums/new.ejs');
 });
 
+// ALBUM INDEX PAGE
+router.get('/', (req, res) => {
+    // find all albums
+    Album.find({}, (error, albums) => {
+        console.log(`${albums}`)
+        res.render('albums/index.ejs', { albums });
+    });
+});
+
 // CREATE A NEW ALBUM
 router.post('/', (req, res) => {
     Album.create(req.body, (error, album) => {
